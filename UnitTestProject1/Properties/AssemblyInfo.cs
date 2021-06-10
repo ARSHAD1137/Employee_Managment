7554d9071187b8edd4f0642374c281eaa76c4d6e
Arshad <arshadpinjari666@gmail.com>
2021-06-10 21:22:13 +0530

    Add project files.

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("76c6076d-1f3d-4ceb-937f-40b268fb116d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Employee_Managment;
+using Employee_Managment.SalaryModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenSalaryDataAbleToUpdateSalaryDetails()
+        {
+            Salary salary = new Salary();
+            SalaryUpdateModel updateModel = new SalaryUpdateModel()
+            {
+                SalaryId = 1,
+                Month = "Jan",
+                EmployeeSalary = 1300,
+                EmployeeId = 2
+            };
+
+            int EmpSalary = salary.UpdateEmployeeSalary(updateModel);
+
+            Assert.AreEqual(updateModel.EmployeeSalary, EmpSalary);
+        }
+    }
+
+
+}
